78940e1b8d8045735860000be215b084c7253830
uswa.r <uswa.r@outlook.com>
2022-09-09 01:51:30 +0500

    done

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-const btns = document.querySelectorAll('.tab-btn');
-const about = document.querySelector('.about');
-const articles = document.querySelector('.content');
-
-about.addEventListener('click', function(e) {
+const about = document.querySelector(".about");
+const btns = document.querySelectorAll(".tab-btn");
+const articles = document.querySelectorAll(".content");
+about.addEventListener("click", function(e) {
     const id = e.target.dataset.id;
     if (id) {
+        // remove selected from other buttons
         btns.forEach(function(btn) {
-            btn.classList.remove('active');
+            btn.classList.remove("active");
         });
-        e.target.classList.add('active');
-
+        e.target.classList.add("active");
+        // hide other articles
         articles.forEach(function(article) {
-            article.classList.remove('active');
+            article.classList.remove("active");
         });
         const element = document.getElementById(id);
-        element.classList.add('active');
+        element.classList.add("active");
     }
 });
