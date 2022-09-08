33321a8da2bbff508b77f1cf12494e3896478ff8
uswa.r <uswa.r@outlook.com>
2022-09-09 01:49:25 +0500

    done

--- script.js
+++ script.js
@@ -7,7 +7,13 @@ about.addEventListener('click', function(e) {
     if (id) {
         btns.forEach(function(btn) {
             btn.classList.remove('active');
-            e.target.classList.add('active');
         });
+        e.target.classList.add('active');
+
+        articles.forEach(function(article) {
+            article.classList.remove('active');
+        });
+        const element = document.getElementById(id);
+        element.classList.add('active');
     }
 });
